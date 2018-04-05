ded95e3a9cf38750f111d59eb56d341c42bd722e
Sanni Kehinde <kensanni@gmail.com>
2018-04-05 12:08:31 +0100

    chore(setup): setup graphl and express server, eslint, nodemon and babel

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+const schema = buildSchema(`
+  type Query {
+    eventAPI: String
+  }
+`)
+
+const root = { eventAPI: () => 'welcome to the event manager API' }
+
+const app = express()
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(4000, console.log('Now browse to localhost:4000/'))
