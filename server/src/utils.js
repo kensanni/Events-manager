504621cd63b312d03c3acef425f278a9ed08d190
kensanni <kensanni@gmail.com>
2018-04-25 12:37:26 +0100

    feature(authentication): implement user signup
    Install bcrypt for hashing user password
    Install JsonWebToken for authentication
    Add AuthPayload resolver to solve issue with selection set

---
+++ server/src/utils.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+
+export const APP_SECRET = 'DEVELOPMENT_TEST'
+
+export const getUserId = (context) => {
+  const Authorization = context.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer', '')
+    const { userId } = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticates')
+}
